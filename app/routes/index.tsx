ed786024af1362b2bbcbefa14d08796f47cabaf1
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2022-04-18 08:36:07 +0530

    Init

---
+++ app/routes/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Index() {
+  return (
+    <React.Fragment>
+      <h1 className="text-3xl bg-green-500 font-bold underline">
+        Hello world!
+      </h1>
+    </React.Fragment>
+  )
+}
